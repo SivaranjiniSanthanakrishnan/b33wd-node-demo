9fbdddc782492f83646a13a6e6487150a64a6a4f
SivaranjiniSanthanakrishnan <43466009+SivaranjiniSanthanakrishnan@users.noreply.github.com>
2022-10-17 12:38:36 +0530

    Update employee.js

--- router/employee.js
+++ router/employee.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const employeeModule = require('../modules/employee');
+const auth = require("../modules/authModule");
 
 router.get('/get', employeeModule.getEmployees);
 
 router.get('/getposts', employeeModule.getPosts);
 
-router.post('/create', employeeModule.createEmployees)
+router.post('/create',auth.authorizeUser, employeeModule.createEmployees)
 
-router.put('/update/:employeeId', employeeModule.updateEmployees)
+router.put('/update/:employeeId', auth.authorizeUser, employeeModule.updateEmployees)
 
-router.delete('/delete/:employeeId', employeeModule.deleteEmployees);
+router.delete('/delete/:employeeId',auth.authorizeUser,  employeeModule.deleteEmployees);
 
 module.exports = router;
